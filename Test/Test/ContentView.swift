68bc4ff2e654728680e5acc1730a58cbf9d97afc
free-your-code <2583042618@qq.com>
2020-08-25 22:35:01 +0800

    first init project

---
+++ Test/Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by 404err on 2020/8/25.
+//  Copyright © 2020 404err. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
